f1aa10ca0fc13a71f50c1b2b62efbc50181fc0bf
tanran <215920697@qq.com>
2018-11-06 16:15:57 +0800

    完善登录注册,解决了一点小问题

--- blogs/router/user.js
+++ blogs/router/user.js
@@ -1,21 +1,17 @@
 const express = require("express");
+const ctrl = require("../controller/user.js");
 const router = express.Router(); //路由
 
 
-
-
-const ctrl = require("../controller/index.js");
-
-
 //登录页
-router.get("/login", ctrl.login);
+router.get("/login", ctrl.handelGetLogin);
 //注册
-router.get("/register", ctrl.register);
+router.get("/register", ctrl.handelGetRegister);
 //注册信息获取
-router.post("/register", ctrl.registers);
+router.post("/register", ctrl.handelPostRegister);
 
 //登录信息获取
-router.post("/login",ctrl.logins);
+router.post("/login",ctrl.handelPostLogin);
 
 module.exports = router;
 
