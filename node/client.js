06f10200aa5fdfab0096d5408c8df04719a256bf
tanran <215920697@qq.com>
2019-06-28 23:47:43 +0800

    tcp

--- node/client.js
+++ node/client.js
@@ -1,4 +1,7 @@
 const net = require('net')
+const type = require('./type')
+
+let nickname = null
 
 const client = net.createConnection({
   host: '127.0.0.1',
@@ -6,16 +9,58 @@ const client = net.createConnection({
 })
 
 client.on('connect', () => {
-  console.log('成功啦')
-
-  client.write('我不好')
-
+  process.stdout.write('请输入昵称: ')
   // 监听客户端输入
   process.stdin.on('data', data => {
-    client.write(data.toString().trim())
+    data = data.toString().trim()
+    if (!nickname) {
+      client.write(JSON.stringify({
+        type: type.login,
+        nickname: data
+      }))
+      return
+    }
+    // 私聊
+    const matches = /^@(\w+)\s(.+)$/.exec(data)
+    if (matches) { // 如果本次消息复合 @xxx xxx
+      return client.write(JSON.stringify({
+        type: type.p2p,
+        nickname: matches[1],
+        message: matches[2]
+      }))
+    }
+    // 群聊
+  client.write(JSON.stringify({
+    type: type.broadcast,
+    message: data
+  }))
   })
 })
 
 client.on('data', data => {
-  console.log(data.toString())
+  data = JSON.parse(data.toString().trim())
+  switch (data.type) {
+    case type.login:
+      if (!data.success) {
+        console.log(`登录失败 ${data.message}`)
+        process.stdout.write('请输入昵称: ')
+      } else {
+        console.log(`登录成功, 当前用户为 ${data.nickname}`)
+        console.log(`当前是第 ${data.sumUsers} 个登录用户`)
+        nickname = data.nickname
+      }
+      break
+    case type.broadcast:
+      console.log(`${data.nickname}: ${data.message}`)
+      break
+    case type.p2p:
+    console.log(`${data.nickname} 对你说: ${data.message}`)
+      break
+    case type.log:
+      console.log(data.message)
+      break
+    default:
+      console.log('未知变量')
+      break
+  }
 })
--- node/server.js
+++ node/server.js
@@ -1,20 +1,85 @@
 const net = require('net')
+const type = require('./type')
 
 const server = net.createServer()
-const clients = []
+const users = []
 
 server.on('connection', clientSocket => {
-  console.log('有新的连接进来了')
-  clients.push(clientSocket)
   clientSocket.on('data', data => {
-    clients.forEach(socket => {
-      if (socket !== clientSocket) {
-        socket.write(data)
-      }
-    })
+    data = JSON.parse(data.toString().trim())
+    switch (data.type) {
+      case type.login:
+        if (users.find(item => item.nickname === data.nickname)) {
+          return clientSocket.write(JSON.stringify({
+            type: type.login,
+            success: false,
+            nickname: data.nickname,
+            message: '昵称已重复'
+          }))
+        }
+        clientSocket.nickname = data.nickname
+        users.push(clientSocket)
+        clientSocket.write(JSON.stringify({
+          type: type.login,
+          success: true,
+          message: '登录成功',
+          sumUsers: users.length,
+          nickname: data.nickname
+        }))
+
+        // 广播登录用户
+        users.forEach(item => {
+          if (item.nickname !== clientSocket.nickname) {
+            item.write(JSON.stringify({
+              type: type.log,
+              success: true,
+              message: `用户${clientSocket.nickname}已上线, 当前在线人数为${users.length}`
+            }))
+          }
+        })
+        break
+      case type.broadcast:
+        users.forEach(item => {
+          item.write(JSON.stringify({
+            type: type.broadcast,
+            nickname: clientSocket.nickname,
+            message: data.message
+          }))
+        })
+        break
+      case type.p2p:
+        const user = users.find(item => item.nickname === data.nickname)
+        if (!user) {
+          return clientSocket.write(JSON.stringify({
+            type: type.p2p,
+            success: false,
+            message: '该用户不存在'
+          }))
+        }
+
+        user.write(JSON.stringify({
+          type: type.p2p,
+          success: true,
+          nickname: clientSocket.nickname,
+          message: data.message
+        }))
+        break
+    }
   })
-  // 通过clientSocket给当前连接的客户端发送数据
-  clientSocket.write('你好啊')
+  // 清除离线用户
+  clientSocket.on('end', () => {
+    const index = users.findIndex(item => item.nickname === clientSocket.nickname)
+    if (index !== -1) {
+      const outUser = users.splice(index, 1)
+      users.forEach(item => {
+        item.write(JSON.stringify({
+          type: type.log,
+          success: true,
+          message: `用户${outUser.nickname}已退出, 当前在线用户为${users.length}`
+        }))
+      })
+    }
+  })
 })
 
 server.listen(3000, () => {
