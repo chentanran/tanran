9bf58b8633ca72086bf6680eb07267280e18a038
chentanran <215920697@qq.com>
2022-09-20 15:50:59 +0800

    vue.js 设计与实现

---
+++ vue.js设计与实现/模板的工作原理.vue
@@ -0,0 +1,17 @@
+<template>
+	<div @click="handler">
+		click me
+	</div>
+</template>
+
+<script>
+export default {
+	data() {},
+	methods: {
+		handler: () => {}
+	},
+	render() {
+		return h('div', { onClick: handler }, 'click me')
+	}
+}
+</script>
