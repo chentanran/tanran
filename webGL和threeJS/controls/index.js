a8ff72bf1ede453672331becd796986eefb97c99
chentanran <215920697@qq.com>
2023-03-09 10:13:20 +0800

    three.js 材质

--- webGL和threeJS/controls/index.js
+++ webGL和threeJS/controls/index.js
@@ -29,6 +29,84 @@ const basicType = {
     getValue: item => item.exponent,
     setValue: (item, value) => item.exponent = +value
   },
+  opacity: {
+    extends: [0, 1],
+    getValue: item => item.opacity,
+    setValue: (item, value) => item.opacity = +value
+  },
+  transparent: {
+    getValue: item => item.transparent,
+    setValue: (item, value) => item.transparent = value
+  },
+  wireframe: {
+    getValue: item => item.wireframe,
+    setValue: (item, value) => item.wireframe = value
+  },
+  visible: {
+    getValue: item => item.visible,
+    setValue: (item, value) => item.visible = value
+  },
+  cameraNear: {
+    extends: [0, 50],
+    getValue: (item, camera) => camera.near,
+    setValue: (item, value, camera) => camera.near = value
+  },
+  cameraFar: {
+    extends: [50, 1000],
+    getValue: (item, camera) => camera.far,
+    setValue: (item, value, camera) => camera.far = value
+  },
+  side: {
+    extends: [['front', 'back', 'double']],
+    getValue: () => 'front',
+    setValue: (item, value) => {
+      switch(value) {
+        case 'front':
+          item.side = THREE.FrontSide
+          break
+        case 'back':
+          item.side = THREE.BackSide
+          break
+        case 'double':
+          item.side = THREE.DoubleSide
+          break
+      }
+    }
+  },
+  // 材料的环境颜色
+  ambient: {
+    method: 'addColor',
+    getValue: (item) => item.ambient.getHex(),
+    setValue: (item, value, camera) => item.ambient = new THREE.Color(value)
+  },
+  // 物体材料本身发出的颜色
+  emissive: {
+    method: 'addColor',
+    getValue: (item) => item.emissive.getHex(),
+    setValue: (item, value, camera) => item.emissive = new THREE.Color(value)
+  },
+  // 设置高亮部分的颜色
+  specular: {
+    method: 'addColor',
+    getValue: (item) => item.specular.getHex(),
+    setValue: (item, value, camera) => item.specular = new THREE.Color(value)
+  },
+  // 设置高亮部门的颜色
+  shininess: {
+    extends: [0, 100],
+    getValue: (item) => item.shininess,
+    setValue: (item, value, camera) => item.shininess = value
+  },
+  red: {
+    extends: [0, 1],
+    getValue: (item) => item.uniforms.r.value,
+    setValue: (item, value, camera) => item.uniforms.r.value = value
+  },
+  alpha: {
+    extends: [0, 1],
+    getValue: (item) => item.uniforms.a.value,
+    setValue: (item, value, camera) => item.uniforms.a.value = value
+  },
 }
 
 const itemType = {
@@ -37,9 +115,16 @@ const itemType = {
   PointLight: ['color', 'intensity', 'distance'], // 点光源
   DirectionalLight: ['color', 'intensity'], // 平行光
   HemisphereLight: ['skyColor', 'groundColor', 'intensity'], // 半球光
+  MeshBasicMaterial: ['color', 'opacity', 'transparent', 'wireframe', 'visible'], // 基础材质
+  MeshDepthMaterial: ['wireframe', 'cameraNear', 'cameraFar'], // 深度材质
+  MeshNormalMaterial: ['opacity', 'transparent', 'wireframe', 'visible', 'side'], // 法向材质
+  MeshLambertMaterial: ['opacity', 'transparent', 'wireframe', 'visible', 'side', 'ambient', 'emissive', 'color'], // 郎伯材质
+  MeshPhongMaterial: ['opacity', 'transparent', 'wireframe', 'visible', 'side', 'ambient', 'emissive', 'color', 'specular', 'shininess'], // Phong材质
+  ShaderMaterial: ['red', 'alpha'], // 着色器材质
 }
 
-function initControls (item) {
+function initControls (item, camera) {
+  console.log(item)
   const typeList = itemType[item.type]
 
   const controls = {}
@@ -51,14 +136,13 @@ function initControls (item) {
 
   for (let i = 0; i < typeList.length; i++) {
     const child = basicType[typeList[i]]
-    console.log(child, 'child')
     if (child) {
-      controls[typeList[i]] = child.getValue(item)
+      controls[typeList[i]] = child.getValue(item, camera)
 
       const childExtends = child.extends || []
 
       gui[child.method || 'add'](controls, typeList[i], ...childExtends).onChange(value => {
-        child.setValue(item, value)
+        child.setValue(item, value, camera)
       })
 
     }
