b108855940928787b278822ededdad2466e1461b
chentanran <215920697@qq.com>
2023-03-11 15:20:17 +0800

    three.js 立方体

--- webGL和threeJS/controls/index.js
+++ webGL和threeJS/controls/index.js
@@ -107,6 +107,121 @@ const basicType = {
     getValue: (item) => item.uniforms.a.value,
     setValue: (item, value, camera) => item.uniforms.a.value = value
   },
+  width: getMeshValue([0, 20], 'width'),
+  height: getMeshValue([0, 20], 'height'),
+  widthSegments: getMeshValue([0, 20], 'widthSegments'),
+  heightSegments:getMeshValue([0, 20], 'heightSegments'),
+  radius:getMeshValue([1, 20], 'radius'),
+  segments:getMeshValue([3, 80], 'segments'),
+  thetaStart:getMeshValue([0, Math.PI * 2], 'thetaStart'),
+  thetaLength:getMeshValue([0, Math.PI * 2], 'thetaLength'),
+  depth:getMeshValue([0, 20], 'depth'),
+  depthSegments:getMeshValue([0, 20], 'depthSegments'),
+  phiStart:getMeshValue([0, Math.PI * 2], 'phiStart'),
+  phiLength:getMeshValue([0, Math.PI * 2], 'phiLength'),
+  radiusTop:getMeshValue([-20, 20], 'radiusTop'),
+  radiusBottom:getMeshValue([-20, 20], 'radiusBottom'),
+  radialSegments:getMeshValue([1, 60], 'radialSegments'),
+  openEnded:getMeshValue([], 'openEnded'),
+  tube:getMeshValue([1, 6], 'tube'),
+  tubularSegments:getMeshValue([0, Math.PI * 2], 'tubularSegments'),
+  arc:getMeshValue([1, 20], 'arc'),
+  heightScale:getMeshValue([0, 5], 'heightScale'),
+  p:getMeshValue([1, 10], 'p'),
+  q:getMeshValue([1, 10], 'q'),
+  size:getMeshValue([1, 10], 'size'),
+  bevelThickness:getMeshValue([1, 30], 'bevelThickness'),
+  bevelSize:getMeshValue([1, 30], 'bevelSize'),
+  bevelEnabled:getMeshValue([], 'bevelEnabled'),
+  bevelSegments:getMeshValue([1, 30], 'bevelSegments'),
+  curveSegments:getMeshValue([1, 30], 'curveSegments'),
+  steps:getMeshValue([1, 10], 'steps'),
+}
+
+const roundValue = {
+  width: 1,
+  height: 1,
+  depth: 1,
+  widthSegments: 1,
+  heightSegments: 1,
+  depthSegments: 1,
+  radialSegments: 1,
+  tubularSegments: 1,
+  detail: 1,
+  size: 1,
+  bevelSegments: 1,
+  curveSegments: 1,
+  steps: 1,
+}
+
+const textOptions = {
+  size: 1,
+  height: 1,
+  weight: 'normal',
+  font: 'helvetiker',
+  bevelThickness: 1,
+  bevelSize: 1,
+  bevelEnabled: false,
+  bevelSegments: 1,
+  curveSegments: 1,
+  steps: 1,
+}
+
+const isPolyhedron = item => item.type === 'PolyhedronGeometry';
+const isFont = item => item.type === 'TextGeometry';
+
+const vertices = [
+  1,1,1,-1,-1,1,-1,1,-1,1,-1,-1,
+]
+
+const indices = [
+  2,1,0,0,3,2,1,3,0,2,3,1
+]
+
+function createMaterial(geometry) {
+  const lambert = new THREE.MeshLambertMaterial({ color: 0xff0000 })
+  const basic = new THREE.MeshBasicMaterial({ wireframe: true })
+
+  return THREE.SceneUtils.createMultiMaterialObject(geometry, [
+    lambert,
+    basic
+  ])
+}
+
+function removeAndAdd(item, value, camera, mesh, scene, controls) {
+  const { x, y, z } = mesh.pointer.rotation
+
+  scene.remove(mesh.pointer)
+  const arg = []
+
+  for (const key in controls) {
+    if (roundValue[key]) {
+      console.log(controls[key], ~~controls[key])
+      controls[key] = ~~controls[key]
+    }
+    arg.push(controls[key])
+  }
+
+  if (isPolyhedron(item)) {
+    arg.unshift(vertices, indices);
+  }
+
+  if (isFont(item)) {
+    mesh.pointer = createMaterial(new THREE[item.type]('123', Object.assign(textOptions, controls)))
+  } else {
+    mesh.pointer = createMaterial(new THREE[item.type](...arg))
+  }
+
+  mesh.pointer.rotation.set(x, y, z);
+  scene.add(mesh.pointer);
+}
+
+function getMeshValue (extend, name) {
+  return {
+    extends: extend,
+    getValue: (item, camera, mesh) => isFont(item) && textOptions[name] !== undefined ? textOptions[name] : mesh.children[0].geometry.parameters[name],
+    setValue: (...arg) => removeAndAdd(...arg)
+  }
 }
 
 const itemType = {
@@ -121,10 +236,18 @@ const itemType = {
   MeshLambertMaterial: ['opacity', 'transparent', 'wireframe', 'visible', 'side', 'ambient', 'emissive', 'color'], // 郎伯材质
   MeshPhongMaterial: ['opacity', 'transparent', 'wireframe', 'visible', 'side', 'ambient', 'emissive', 'color', 'specular', 'shininess'], // Phong材质
   ShaderMaterial: ['red', 'alpha'], // 着色器材质
+  PlaneGeometry: ['width', 'height', 'widthSegments', 'heightSegments'], // 二维平面几何体
+  CircleGeometry: ['radius', 'segments', 'thetaStart', 'thetaLength'], // 二维圆
+  BoxGeometry: ['width', 'height', 'depth', 'widthSegments', 'heightSegments', 'depthSegments'], // 立方体
+  SphereGeometry: ['radius', 'widthSegments', 'heightSegments', 'phiStart', 'phiLength', 'thetaStart', 'thetaLength'], // 球体
+  CylinderGeometry: ['radiusTop', 'radiusBottom', 'height', 'radialSegments', 'heightSegments', 'openEnded'], // 圆柱体
+  TorusGeometry: ['radius', 'tube', 'radialSegments', 'tubularSegments', 'arc'], // 圆环
+  TorusKnotGeometry: ['radius', 'tube', 'radialSegments', 'tubularSegments', 'p', 'q', 'heightScale'], // 扭结
+  TextGeometry: ['size', 'bevelThickness', 'bevelSize', 'bevelEnabled', 'bevelSegments', 'curveSegments', 'steps'], // 文字
 }
 
-function initControls (item, camera) {
-  console.log(item)
+function initControls (item, camera, mesh, scene) {
+
   const typeList = itemType[item.type]
 
   const controls = {}
@@ -137,12 +260,12 @@ function initControls (item, camera) {
   for (let i = 0; i < typeList.length; i++) {
     const child = basicType[typeList[i]]
     if (child) {
-      controls[typeList[i]] = child.getValue(item, camera)
+      controls[typeList[i]] = child.getValue(item, camera, mesh.pointer)
 
       const childExtends = child.extends || []
 
       gui[child.method || 'add'](controls, typeList[i], ...childExtends).onChange(value => {
-        child.setValue(item, value, camera)
+        child.setValue(item, value, camera, mesh, scene, controls)
       })
 
     }
