fc9faf2c903e7c4034c2174a2675e0fe7b9017ae
chentanran <215920697@qq.com>
2024-06-06 16:33:42 +0800

    boarding

---
+++ react通关秘籍/locate-code-test/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+// @ts-ignore
+import { ClickToComponent } from 'click-to-react-component';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    <ClickToComponent editor="vscode-insiders" />
+    <App />
+  </>
+)
