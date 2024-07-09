39a6cc52e626b3ed0e6e66108513070873095eb3
chentanran <215920697@qq.com>
2024-07-09 09:54:09 +0800

    row col

---
+++ vue3企业级项目实战课/monorepo/packages/components/src/col/col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="{ [baseClassName]: true, [spanClassName]: true }">
+    <slot v-if="$slots.default"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { prefixName } from '../theme';
+
+const props = defineProps({
+  span: Number
+});
+
+function getSpan(propspan: number | undefined): number {
+  if (typeof propspan === 'number') {
+    const span: number = Math.ceil(Number(propspan));
+    if (span >= 1 && span <= 24) {
+      return span;
+    }
+  }
+
+  return 1;
+}
+
+const baseClassName = `${prefixName}-col`;
+const spanClassName = `${prefixName}-col-${getSpan(props.span)}`;
+</script>
+
+<style lang="less" scoped></style>
--- vue3企业级项目实战课/monorepo/packages/components/src/index.ts
+++ vue3企业级项目实战课/monorepo/packages/components/src/index.ts
@@ -2,5 +2,7 @@ import Button from './button';
 import Box from './box';
 import Message from './message';
 import Dialog from './dialog';
+import Row from './row';
+import Col from './col';
 
-export { Button, Box, Message, Dialog };
+export { Button, Box, Message, Dialog, Row, Col };
